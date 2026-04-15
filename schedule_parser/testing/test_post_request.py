0b9774f88936ef55adf690bf7260902760045457
Shurik <sanya.kucheryavvyh@bk.ru>
2026-04-15 12:42:07 +0300

    tests: implemented test post auth

--- schedule_parser/testing/test_post_request.py
+++ schedule_parser/testing/test_post_request.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from pathlib import Path
 
 import pytest
 import pytest_asyncio
@@ -19,6 +20,7 @@ APPLICATION_KEY = os.getenv('APPLICATION_KEY')
 Базовое мокирование post запросов
 """
 
+
 class TestPostRequest:
     @respx.mock
     @pytest.mark.asyncio
@@ -30,9 +32,34 @@ class TestPostRequest:
         async with httpx.AsyncClient() as client:
             resp = await client.post(
                 'https://msapi.top-academy.ru/api/v2/auth/login',
-                     json={"username": USERNAME, "password": PASSWORD})
+                     json={
+                         'application_key': APPLICATION_KEY,
+                         "username": USERNAME,
+                         "password": PASSWORD})
 
             assert resp.status_code == 200
             assert resp.json()['access_token'] == 'test-access-token'
             assert resp.json()['refresh_token'] == 'test-token'
+            assert resp.json() != {}
+            assert resp.json() is not None
+
+
+    @respx.mock
+    @pytest.mark.asyncio
+    async def test_error_post(self):
+        respx.post("https://msapi.top-academy.ru/api/v2/auth/login").mock(
+            return_value=httpx.Response(403, json={'Warning': "некорректный формат отправленных данных"})
+        )
+
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                "https://msapi.top-academy.ru/api/v2/auth/login",
+                json={
+                    'application_key': 'wejkfnejnfjkenf',
+                    'username': 'awkfnwf',
+                    'password': 'jewjfiejfije'
+                }
+            )
+            assert response.status_code == 403
+            assert response.json() == {'Warning': "некорректный формат отправленных данных"}
 
